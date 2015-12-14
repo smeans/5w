a7fdb43a08ae6438e473641dd3f42269c9480df8
Arif Widi Nugroho <arif@sainsmograf.com>
2015-12-14 19:10:00 +0700

    added some test views that can be used to help finding correct prefix for an id field

---
+++ views/test-field-query/map.js
@@ -0,0 +1,12 @@
+function(doc) {
+  
+  function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+  }
+
+  for (var key in doc) {
+    if (doc.hasOwnProperty(key) && endsWith(key, 'Id')) {
+      emit([key, doc[key]], 1);
+    }
+  }
+}
