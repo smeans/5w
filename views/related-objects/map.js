bb213949525f50a455aff7e162acb6947b730388
Arif Widi Nugroho <arif@sainsmograf.com>
2016-10-12 16:31:34 +0700

    updated related views to include supports for document_link_list field

--- views/related-objects/map.js
+++ views/related-objects/map.js
@@ -16,27 +16,40 @@ function (doc) {
     return str.indexOf(suffix, str.length - suffix.length) !== -1;
   }
 
+  var isValidId = function (id) {
+    if (typeof id != 'string') return false;
+    if (id == 'null_null') return false;
+
+    var idSplit = id.split('_');
+    if (idSplit.length == 2) {
+      return true;
+    }
+
+    return false;
+  }
+
+  if (!isValidId(doc._id)) {
+    return;
+  }
+
   for (var key in doc) {
-    if (doc.hasOwnProperty(key) && endsWith(key, 'Id')) {
-      if (getType(doc[key])) {
-        emit([doc._id, getType(doc[key])], {_id: doc[key]});
+    if (doc.hasOwnProperty(key)) {
+      if (Array.isArray(doc[key])) {
+        for (var i = 0; i < doc[key].length; i++) {
+          var docId = doc[key][i];
+          if (isValidId(docId)) {
+            emit([doc._id, getType(docId)], {_id: docId});
+            emit([docId, getType(doc._id)], {_id: doc._id});
+          }
+        }
       }
-      if (getType(doc._id)) {
-        emit([doc[key], getType(doc._id)], {_id: doc._id});
+      else if (endsWith(key, 'Id')) {
+        if (isValidId(doc[key])) {
+          emit([doc._id, getType(doc[key])], {_id: doc[key]});
+          emit([doc[key], getType(doc._id)], {_id: doc._id});
+        }
       }
     }
   }
 
-  if ('$5w_related' in doc) {
-    var ra= doc.$5w_related;
-
-    for (var i = 0; i < ra.length; i++) {
-      if (getType(ra[i])) {
-        emit([doc._id, getType(ra[i])], {_id: ra[i]});
-      }
-      if (getType(doc._id)) {
-        emit([ra[i], getType(doc._id)], {_id: doc._id});
-      }
-    }
-  }
 }
--- views/related-types/map.js
+++ views/related-types/map.js
@@ -12,31 +12,44 @@ function (doc) {
     return idSplit[0];
   }
 
+  var isValidId = function (id) {
+    if (typeof id != 'string') return false;
+    if (id == 'null_null') return false;
+
+    var idSplit = id.split('_');
+    if (idSplit.length == 2) {
+      return true;
+    }
+
+    return false;
+  }
+
   var endsWith = function (str, suffix) {
     return str.indexOf(suffix, str.length - suffix.length) !== -1;
   }
 
+  if (!isValidId(doc._id)) {
+    return;
+  }
+
   for (var key in doc) {
-    if (doc.hasOwnProperty(key) && endsWith(key, 'Id')) {
-      if (getType(doc[key]) && getType(doc[key]) != 'null') {
-        emit([doc._id, getType(doc[key])], 1);
+    if (doc.hasOwnProperty(key)) {
+      if (Array.isArray(doc[key])) {
+        for (var i = 0; i < doc[key].length; i++) {
+          var docId = doc[key][i];
+          if (isValidId(docId)) {
+            emit([doc._id, getType(docId)], 1);
+            emit([docId, getType(doc._id)], 1);
+          }
+        }
       }
-      if (getType(doc._id) && getType(doc._id) != 'null') {
-        emit([doc[key], getType(doc._id)], 1);
+      else if (endsWith(key, 'Id')) {
+        if (isValidId(doc[key])) {
+          emit([doc._id, getType(doc[key])], 1);
+          emit([doc[key], getType(doc._id)], 1);
+        }
       }
     }
   }
 
-  if ('$5w_related' in doc) {
-    var ra= doc.$5w_related;
-
-    for (var i = 0; i < ra.length; i++) {
-      if (getType(ra[i]) && getType(ra[i]) != 'null') {
-        emit([doc._id, getType(ra[i])], 1);
-      }
-      if (getType(doc._id) && getType(doc._id) != 'null') {
-        emit([ra[i], getType(doc._id)], 1);
-      }
-    }
-  }
 }
