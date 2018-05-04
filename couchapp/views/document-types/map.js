3650fbfec05ffcbbe49efd669496c8b3d6b4bb9a
Scott Means <smeans@gmail.com>
2018-05-03 22:20:17 -0400

    include types with no instance documents

--- couchapp/views/document-types/map.js
+++ couchapp/views/document-types/map.js
@@ -1,11 +1,14 @@
 function (doc) {
-  var ica = doc._id.split('_');
-
   if ('ObjectType' in doc && doc.ObjectType) {
     emit(doc.ObjectType, 1);
   }
 
   if ('allow_create' in doc) {
+    if (doc._id.indexOf('$5w_proto_') == 0) {
+      var ica = doc._id.split('_');
+      emit(ica[2], 0);
+    }
+
     for (var i = 0; i < doc.allow_create.length; i++) {
       emit(doc.allow_create[i], 0);
     }
